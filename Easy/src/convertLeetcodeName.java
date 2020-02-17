9194b9bf0162aff0fdc22eb34c170fbe88ecab80
AceDJH <460745177@qq.com>
2020-02-17 17:37:20 +0800

    手动改名字太麻烦了，因此写了个用来转换名字的

---
+++ Easy/src/convertLeetcodeName.java
@@ -0,0 +1,19 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/2/17 17:26
+ * 用来转换Leetcode的名字，方便起名
+ */
+import java.util.Scanner;
+public class convertLeetcodeName{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入题目名,当前为Easy类：");
+        String name = sc.nextLine();
+        String str1 = name.replaceAll(". ", "_");
+        String str2 = str1.replace(' ', '_');
+        // 更改名字中的难度时，修改degree
+        String degree = "E_";
+        System.out.println(degree + str2);
+
+    }
+}
