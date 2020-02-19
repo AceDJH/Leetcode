ead861b8a1c729f45a07d63a2b107218a68ecb0c
AceDJH <460745177@qq.com>
2020-02-19 11:40:31 +0800

    136. 只出现一次的数字

--- Easy/src/E_136_Single_Number.java
+++ Easy/src/E_136_Single_Number.java
@@ -25,7 +25,7 @@ import java.util.Arrays;
  * 链接：https://leetcode-cn.com/problems/single-number
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class E_136_Singl_Number {
+public class E_136_Single_Number {
 }
 
 /*class Solution136 {
