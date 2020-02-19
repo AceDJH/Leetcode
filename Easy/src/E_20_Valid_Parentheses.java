6119b7b8e28ef2f0dd9d638a295ae4bf48068ee5
AceDJH <460745177@qq.com>
2020-02-19 19:15:26 +0800

    20. 有效的括号

---
+++ Easy/src/E_20_Valid_Parentheses.java
@@ -0,0 +1,67 @@
+import java.util.Stack;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/2/19 18:54
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/valid-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_20_Valid_Parentheses {
+}
+
+class Solution20 {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0)
+            return false;
+        Stack<Character> chars = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
+                chars.push(s.charAt(i));
+            else if (!chars.empty()){
+                if (s.charAt(i) == ')' && chars.pop() != '('){
+                    return false;
+                }
+                if (s.charAt(i) == '}' && chars.pop() != '{'){
+                    return false;
+                }
+                if (s.charAt(i) == ']' && chars.pop() != '['){
+                    return false;
+                }
+            }else {
+                return false;
+            }
+        }
+        return chars.empty();
+    }
+}
