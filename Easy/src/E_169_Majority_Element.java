3cfe9e5cd71fe6a78429fa3071a7e2f7d3614ccc
AceDJH <460745177@qq.com>
2020-02-19 11:42:13 +0800

    169. 多数元素

--- Easy/src/E_169_Majority_Element.java
+++ Easy/src/E_169_Majority_Element.java
@@ -21,7 +21,7 @@ import java.util.Arrays;
  * 链接：https://leetcode-cn.com/problems/majority-element
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class E_169_Majorit_Element {
+public class E_169_Majority_Element {
 }
 
 class Solution169 {
