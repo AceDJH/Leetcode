5dfbf3b7e4ca8f8d49059c512daae042a6316d47
AceDJH <460745177@qq.com>
2020-02-19 13:08:02 +0800

    88. 合并两个有序数组

---
+++ Easy/src/E_88_Merge_Sorted_Array.java
@@ -0,0 +1,50 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/2/19 12:07
+ * 88. 合并两个有序数组
+ * 给定两个有序整数数组 nums1 和 nums2，将 nums2 合并到 nums1 中，使得 num1 成为一个有序数组。
+ * <p>
+ * 说明:
+ * <p>
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n。
+ * 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。
+ * 示例:
+ * <p>
+ * 输入:
+ * nums1 = [1,2,3,0,0,0], m = 3
+ * nums2 = [2,5,6],       n = 3
+ * <p>
+ * 输出: [1,2,2,3,5,6]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_88_Merge_Sorted_Array {
+}
+
+/*偷了个懒，根本不用管题中条件，用函数就完事了
+class Solution88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = 0; i < nums2.length; i++) {
+            nums1[m + i] = nums2[i];
+        }
+        Arrays.sort(nums1);
+    }
+}*/
+
+/*思考后的做法
+class Solution88 {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1;
+        int p2 = n - 1;
+        int p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            nums1[p--] = nums1[p1] >= nums2[p2] ? nums1[p1--] : nums2[p2--];
+        }
+
+        while (p2 >= 0) {
+            nums1[p--] = nums2[p2--];
+        }
+    }
+}*/
