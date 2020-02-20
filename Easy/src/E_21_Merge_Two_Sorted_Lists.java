3048be5babb019ace85e142302ec75d0fcbeb114
AceDJH <460745177@qq.com>
2020-02-20 18:51:27 +0800

    21. 合并两个有序链表

---
+++ Easy/src/E_21_Merge_Two_Sorted_Lists.java
@@ -0,0 +1,61 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/2/20 18:16
+ * 21. 合并两个有序链表
+ * 将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_21_Merge_Two_Sorted_Lists {
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/*迭代
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode head = prehead;
+        while (l1 != null && l2 != null){
+            if (l1.val <= l2.val){
+                head.next = l1;
+                l1 = l1.next;
+            }else {
+                head.next = l2;
+                l2 = l2.next;
+            }
+            head = head.next;
+        }
+        head.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+}*/
+// 递归
+class Solution21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        else if (l2 == null)
+            return l1;
+        else if (l1.val <= l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else{
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+}
