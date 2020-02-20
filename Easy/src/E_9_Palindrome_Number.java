ab65eb3bf5c71a0e439aa86bd5f1f3567bf04b06
AceDJH <460745177@qq.com>
2020-02-20 19:07:02 +0800

    9. 回文数

---
+++ Easy/src/E_9_Palindrome_Number.java
@@ -0,0 +1,59 @@
+import java.util.Arrays;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/2/20 18:52
+ * 9. 回文数
+ * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ *
+ * 示例 1:
+ *
+ * 输入: 121
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: -121
+ * 输出: false
+ * 解释: 从左向右读, 为 -121 。 从右向左读, 为 121- 。因此它不是一个回文数。
+ * 示例 3:
+ *
+ * 输入: 10
+ * 输出: false
+ * 解释: 从右向左读, 为 01 。因此它不是一个回文数。
+ * 进阶:
+ *
+ * 你能不将整数转为字符串来解决这个问题吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindrome-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_9_Palindrome_Number {
+    public static void main(String[] args) {
+        System.out.println(new Solution9().isPalindrome(121));
+    }
+}
+
+/*使用字符串
+class Solution9 {
+    public boolean isPalindrome(int x) {
+        String a = String.valueOf(x);
+        StringBuilder builder = new StringBuilder(a);
+        String b = String.valueOf(builder.reverse());
+        return a.equals(b);
+    }
+}*/
+
+class Solution9 {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while(x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber/10;
+    }
+}
