e391216e042358d8699e46eb1167101a079e4cb2
AceDJH <460745177@qq.com>
2020-02-26 22:50:37 +0800

    118. 杨辉三角

---
+++ Easy/src/E_118_Pascal_Triangle.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/2/26 22:18
+ * 118. 杨辉三角
+ * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *
+ *
+ *
+ * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_118_Pascal_Triangle {
+    public static void main(String[] args) {
+        System.out.println(new Solution118().generate(5).toString());
+    }
+}
+
+class Solution118 {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = 1; j <= i ; j++) {
+                if (j == 1 || j == i)
+                    list.add(1);
+                else {
+                    int num = lists.get(i - 2).get(j - 2) + lists.get(i - 2).get(j - 1);
+                    list.add(num);
+                }
+            }
+            lists.add(list);
+        }
+        return lists;
+    }
+}
