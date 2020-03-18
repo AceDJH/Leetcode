dd26747efaa6553c5e4e439a0ebfabbdf86f211d
AceDJH <460745177@qq.com>
2020-03-18 16:30:23 +0800

    836. 矩形重叠

---
+++ Easy/src/E_836_Rectangle_Overlap.java
@@ -0,0 +1,67 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/3/18 14:35
+ * 836. 矩形重叠
+ * 矩形以列表 [x1, y1, x2, y2] 的形式表示，其中 (x1, y1) 为左下角的坐标，(x2, y2) 是右上角的坐标。
+ *
+ * 如果相交的面积为正，则称两矩形重叠。需要明确的是，只在角或边接触的两个矩形不构成重叠。
+ *
+ * 给出两个矩形，判断它们是否重叠并返回结果。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+ * 输出：true
+ * 示例 2：
+ *
+ * 输入：rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 两个矩形 rec1 和 rec2 都以含有四个整数的列表的形式给出。
+ * 矩形中的所有坐标都处于 -10^9 和 10^9 之间。
+ * x 轴默认指向右，y 轴默认指向上。
+ * 你可以仅考虑矩形是正放的情况。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rectangle-overlap
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class E_836_Rectangle_Overlap {
+}
+
+/*class Solution836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        // 排除边缘区域
+        if (rec1[2] == rec2[0] || rec1[3] == rec2[1] || rec1[0] == rec2[2] || rec1[1] == rec2[3])
+            return false;
+        // 三角形一的右上角大于三角二的左下角
+        // 三角形一的左下角小于三角二的右上角
+        if(rec1[2] >= rec2[0] && rec1[3] >= rec2[1] && rec1[0] <= rec2[2] && rec1[1] <= rec2[3])
+            return true;
+        return false;
+    }
+}*/
+
+// 使用排除法，写出不可能重叠的情况，与我之前写的false已经很接近了。
+/*
+class Solution836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return !(rec1[2] <= rec2[0] ||   // left
+                rec1[3] <= rec2[1] ||   // bottom
+                rec1[0] >= rec2[2] ||   // right
+                rec1[1] >= rec2[3]);    // top
+    }
+}*/
+
+// 利用两矩形重叠后的重叠区域一定为矩形，然后判断投影到轴上的线段是否有重叠，有则满足要求
+class Solution836 {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        return (Math.min(rec1[2], rec2[2]) > Math.max(rec1[0], rec2[0])) &&
+                Math.min(rec1[3], rec2[3]) > Math.max(rec1[1], rec2[1]);
+    }
+}
