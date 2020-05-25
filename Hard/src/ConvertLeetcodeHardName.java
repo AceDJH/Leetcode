3f3373b9ff654c92113a7babb3c594dd03eea5d0
AceDJH <460745177@qq.com>
2020-05-25 17:05:40 +0800

    命名

--- Hard/src/ConvertLeetcodeHardName.java
+++ Hard/src/ConvertLeetcodeHardName.java
@@ -1,21 +1,20 @@
+import java.util.Scanner;
+
 /**
  * @Author AceDJH
- * @Date 2020/2/17 17:26
- * 用来转换Leetcode的名字，方便起名
+ * @Date 2020/5/25 17:02
  */
-import java.util.Scanner;
-public class convertLeetcodeName{
+public class ConvertLeetcodeHardName {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("请输入题目名,当前为Easy类：");
+        System.out.println("请输入题目名,当前为Hard类：");
         String name = sc.nextLine();
         StringBuilder stringBuilder = new StringBuilder(name);
         int index = stringBuilder.indexOf(".");
         stringBuilder.deleteCharAt(index);
         String str2 = stringBuilder.toString().replace(' ', '_');
         // 更改名字中的难度时，修改degree
-        String degree = "E_";
+        String degree = "H_";
         System.out.println(degree + str2);
-
     }
 }
