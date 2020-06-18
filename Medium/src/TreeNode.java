8f0f2839dea36884709edc16564886066aee46fc
AceDJH <460745177@qq.com>
2020-06-18 11:31:16 +0800

    102. 二叉树的层序遍历

---
+++ Medium/src/TreeNode.java
@@ -0,0 +1,12 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/6/18 9:14
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val){
+        this.val = val;
+    }
+}
