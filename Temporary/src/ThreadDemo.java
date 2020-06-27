515f87b07fcc65b5c36e45cbcebe3288ebd84e6c
AceDJH <460745177@qq.com>
2020-06-27 23:48:27 +0800

    多线程

---
+++ Temporary/src/ThreadDemo.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/6/18 10:13
+ */
+public class ThreadDemo implements Runnable{
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+
+    private static Integer currentCount = 0;
+
+    private static final Integer MAX_COUNT = 30;
+
+    private static String [] chars = {"a", "b", "c"};
+
+    private String name;
+
+    public ThreadDemo(String name) {
+        this.name =  name;
+    }
+
+    @Override
+    public void run() {
+        while(currentCount<MAX_COUNT){
+            while(this.name.equals(chars[currentCount%3]))
+                printAndPlusOne(this.name + "\t" + currentCount);
+            try {
+                cyclicBarrier.await();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printAndPlusOne(String name){
+        System.out.println(name);
+        currentCount ++;
+    }
+
+    public static void main(String [] args){
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 3, 20, TimeUnit.MINUTES, new LinkedBlockingQueue<Runnable>());
+        threadPoolExecutor.execute(new ThreadDemo("a"));
+        threadPoolExecutor.execute(new ThreadDemo("b"));
+        threadPoolExecutor.execute(new ThreadDemo("c"));
+        threadPoolExecutor.shutdown();
+    }
+}
+
---
+++ Temporary/src/ThreadDemo1.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.*;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/6/18 10:15
+ */
+public class ThreadDemo1 implements Runnable {
+    // static很关键
+    private char name;
+    private static CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+    private char[] chars = new char[]{'a', 'b', 'c'};
+    private static int count = 0;
+    private static final Integer MAX_COUNT = 30;
+    public ThreadDemo1(char name){
+        this.name = name;
+    }
+    @Override
+    public void run() {
+        while (count < MAX_COUNT){
+            if (name == chars[count % 3]){
+                System.out.println(name + " " + count);
+                count++;
+            }
+            try {
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(3, 3, 20, TimeUnit.MINUTES, new LinkedBlockingDeque<>());
+        threadPoolExecutor.execute(new ThreadDemo1('a'));
+        threadPoolExecutor.execute(new ThreadDemo1('b'));
+        threadPoolExecutor.execute(new ThreadDemo1('c'));
+        threadPoolExecutor.shutdown();
+    }
+}
