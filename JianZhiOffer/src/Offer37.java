c71d0230c9d4adcadeb89aa4711e62248a252575
AceDJH <460745177@qq.com>
2020-12-07 23:12:20 +0800

    剑指 Offer 37. 序列化二叉树

---
+++ JianZhiOffer/src/Offer37.java
@@ -0,0 +1,74 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @Author AceDJH
+ * @Date 2020/12/6 21:58
+ * 剑指 Offer 37. 序列化二叉树
+ * 请实现两个函数，分别用来序列化和反序列化二叉树。
+ *
+ * 示例:
+ *
+ * 你可以将以下二叉树：
+ *
+ *     1
+ *    / \
+ *   2   3
+ *      / \
+ *     4   5
+ *
+ * 序列化为 "[1,2,3,null,null,4,5]"
+ */
+public class Offer37 {
+}
+
+class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null){
+            return "[]";
+        }
+        StringBuilder res = new StringBuilder("[");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if (node != null){
+                res.append(node.val).append(",");
+                queue.add(node.left);
+                queue.add(node.right);
+            }else {
+                res.append("null,");
+            }
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append("]");
+        return res.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if ("[]".equals(data)){
+            return null;
+        }
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        while (!queue.isEmpty()){
+            TreeNode node = queue.poll();
+            if (!"null".equals(vals[i])){
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!"null".equals(vals[i])){
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
