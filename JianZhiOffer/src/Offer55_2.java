c22a16a73bdfc206f3fdc50fc5ea83fe869c4c7b
AceDJH <460745177@qq.com>
2021-01-04 09:52:05 +0800

    剑指 Offer 55 - II. 平衡二叉树

--- JianZhiOffer/src/Offer55_2.java
+++ JianZhiOffer/src/Offer55_2.java
@@ -86,3 +86,24 @@ class Solution55_2Better1 {
         return left > right ? left + 1 : right + 1;
     }
 }
+
+class Solution55_2Better2 {
+    public boolean isBalanced(TreeNode root) {
+        return recur(root) != -1;
+    }
+
+    public int recur(TreeNode root){
+        if (root == null){
+            return 0;
+        }
+        int left = recur(root.left);
+        if (left == -1){
+            return -1;
+        }
+        int right = recur(root.right);
+        if (right == -1){
+            return -1;
+        }
+        return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
+    }
+}
