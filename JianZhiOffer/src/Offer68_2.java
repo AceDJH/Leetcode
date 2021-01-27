13c36a8547ad42a8ce399faec865c5c1893de091
AceDJH <460745177@qq.com>
2021-01-27 16:38:34 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先

--- JianZhiOffer/src/Offer68_2.java
+++ JianZhiOffer/src/Offer68_2.java
@@ -86,3 +86,20 @@ class Solution68_2 {
         return isFind;
     }
 }
+
+class Solution68_2Better {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q){
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left == null){
+            return right;
+        }
+        if (right == null){
+            return left;
+        }
+        return root;
+    }
+}
