dfbd307d4fc2bd0050c1ff8ed6b8b07519e5aa3a
AceDJH <460745177@qq.com>
2021-01-27 16:12:35 +0800

    剑指 Offer 68 - II. 二叉树的最近公共祖先(这里的递归写法可以多看看)

---
+++ JianZhiOffer/src/Offer68_2.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author AceDJH
+ * @Date 2021/1/27 14:49
+ * 剑指 Offer 68 - II. 二叉树的最近公共祖先
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ *
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ * 说明:
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ */
+public class Offer68_2 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(6);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(8);
+        TreeNode node3 = new TreeNode(0);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(7);
+        TreeNode node6 = new TreeNode(9);
+        TreeNode node7 = new TreeNode(3);
+        TreeNode node8 = new TreeNode(5);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+        node4.left = node7;
+        node4.right = node8;
+        System.out.println(new Solution68_2().lowestCommonAncestor(root, node1, node8).val);
+    }
+}
+
+class Solution68_2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> pPath = new ArrayList<>();
+        ArrayList<TreeNode> qPath = new ArrayList<>();
+        boolean pFind = findPath(root, p, pPath);
+        boolean qFind = findPath(root, q, qPath);
+        if (pFind && qFind){
+            for (int i = pPath.size() - 1; i >= 0; i--) {
+                for (int j = qPath.size() - 1; j >= 0; j--) {
+                    if (pPath.get(i).equals(qPath.get(j))){
+                        return pPath.get(i);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public boolean findPath(TreeNode root, TreeNode node, List<TreeNode> list){
+        if (root == null){
+            return false;
+        }
+        list.add(root);
+        if (root == node){
+            return true;
+        }
+        boolean isFind = findPath(root.left, node, list) || findPath(root.right, node, list);
+        if (!isFind){
+            list.remove(list.size() - 1);
+        }
+        return isFind;
+    }
+}
