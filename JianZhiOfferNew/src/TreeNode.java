528920e8979cdbdf771512118d7b91ae16ebb336
AceDJH <460745177@qq.com>
2021-02-07 18:19:23 +0800

    二刷剑指，创建的新模块

---
+++ JianZhiOfferNew/src/TreeNode.java
@@ -0,0 +1,13 @@
+/**
+ * @Author AceDJH
+ * @Date 2020/5/22 11:22
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
---
+++ JianZhiOfferNew/src/TreeTest.java
@@ -0,0 +1,36 @@
+/**
+ * @Author AceDJH
+ * @Date 2021/1/29 11:30
+ *
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \    / \
+ *         7    2  5   1
+ */
+public class TreeTest {
+    public static TreeNode getTree(){
+        TreeNode root = new TreeNode(5);
+        TreeNode node1 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(8);
+        TreeNode node3 = new TreeNode(11);
+        TreeNode node4 = new TreeNode(13);
+        TreeNode node5 = new TreeNode(4);
+        TreeNode node6 = new TreeNode(7);
+        TreeNode node7 = new TreeNode(2);
+        TreeNode node8 = new TreeNode(5);
+        TreeNode node9 = new TreeNode(1);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.left = node6;
+        node3.right = node7;
+        node5.left = node8;
+        node5.right = node9;
+        return root;
+    }
+}
