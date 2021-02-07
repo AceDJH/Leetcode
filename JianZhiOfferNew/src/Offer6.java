e85de994338ad5b63cc17d703ba1bfe2d597b5c0
AceDJH <460745177@qq.com>
2021-02-07 20:32:57 +0800

    剑指 Offer 06. 从尾到头打印链表(新增递归解法)

--- JianZhiOfferNew/src/Offer6.java
+++ JianZhiOfferNew/src/Offer6.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 /**
@@ -22,5 +24,29 @@ class Solution6 {
             nums[i] = stack.pop();
         }
         return nums;
+    }
+}
+
+class Solution6Other {
+    List<Integer> list = new ArrayList<>();
+    public int[] reversePrint(ListNode head) {
+        if (head == null){
+            return new int[]{};
+        }
+        recur(head);
+        int[] nums = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            nums[i] = list.get(i);
+        }
+        return nums;
+    }
+
+    public void recur(ListNode node){
+        if (node != null){
+            if (node.next != null){
+                recur(node.next);
+            }
+            list.add(node.val);
+        }
     }
 }
