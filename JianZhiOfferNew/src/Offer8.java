5c6a84ecd0df333d0f0f4596321df169b3804f7d
AceDJH <460745177@qq.com>
2021-03-09 15:23:55 +0800

    剑指 Offer 09. 用两个栈实现队列

---
+++ JianZhiOfferNew/src/Offer8.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+/**
+ * @Author AceDJH
+ * @Date 2021/3/9 15:15
+ */
+public class Offer8 {
+}
+
+class CQueue {
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+    public CQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void appendTail(int value) {
+        stack2.push(value);
+    }
+
+    public int deleteHead() {
+        if (!stack1.isEmpty()){
+            return stack1.pop();
+        }
+        if (!stack2.isEmpty()){
+            while (!stack2.isEmpty()){
+                stack1.push(stack2.pop());
+            }
+            return stack1.pop();
+        }
+        return -1;
+    }
+}
