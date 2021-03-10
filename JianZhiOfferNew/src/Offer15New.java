55ed964c82572d98447fcae23deb241847fae696
AceDJH <460745177@qq.com>
2021-03-10 16:01:45 +0800

    剑指 Offer 15. 二进制中1的个数

---
+++ JianZhiOfferNew/src/Offer15New.java
@@ -0,0 +1,20 @@
+/**
+ * @Author AceDJH
+ * @Date 2021/3/10 15:58
+ */
+public class Offer15New {
+}
+
+public class Solution15New {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0){
+            if ((n & 1) == 1){
+                count++;
+            }
+            n = n >>> 1;
+        }
+        return count;
+    }
+}
