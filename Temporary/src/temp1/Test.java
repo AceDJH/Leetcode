c783ed04d9dd7802b8c31d70dd9ed2dc42223097
AceDJH <460745177@qq.com>
2021-06-22 08:59:41 +0800

    测试foreach的debug（可以）

--- Temporary/src/temp1/Test.java
+++ Temporary/src/temp1/Test.java
@@ -1,9 +1,22 @@
 package temp1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @Author AceDJH
  * @Date 2020/11/19 13:44
  */
 public class Test {
-    
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(Math.abs(new Random().nextInt() % 100));
+            System.out.println(list.get(i));
+        }
+        list.stream().forEach(num -> { int a = num + 3; int b = num + 5; System.out.println(num + 100);
+        });
+        int a = 3;
+    }
 }
